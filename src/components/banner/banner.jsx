cfb35d94541e120fb70fee069fbda8c330a2cdd1
Thkt18 <110399102+Thkt18@users.noreply.github.com>
2023-03-01 14:53:15 +0100

    Banner en Nav, mobile first

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,12 +1,13 @@
-// import BannerImg from '../../assets/banner.png';
 import styled from 'styled-components';
+import { media } from '../../style/breakpoint';
+import theme from '../../style/theme';
 
 function Banner(props) {
 
   return (
 
     <DIV className='banner'>
-      <IMG className="banner_img" src={props.img} alt="BAckground banner" />
+      <IMG className="banner_img" src={props.img} alt="Background banner" />
       <TEXT>
       <TITRE className="banner_text">{props.title}</TITRE>
       </TEXT>
@@ -17,39 +18,35 @@ function Banner(props) {
 
 export default Banner;
 
+
 const TITRE = styled.p`
+    font-size: 24px;
+    line-height: 100%;
+    width: 222px;
+    height: 48px;
     font-weight: 500;
-    font-size: 48px;
-    line-height: 68.45px;
-    margin-right: 0.25em;
-
-    @media (min-width: 345px) and (max-width: 767px) {
-        font-size: 24px;
-        line-height: 10px;
-    }
-
-    @media (min-width: 768px) and (max-width: 1247px) {
-        font-size: 34px;
-    }
+        ${media.desktop} {
+            font-size: 48px;
+            width: 80%;
+        }
 `
 
 const TEXT = styled.div`
-    color: #fff;
-    display: flex;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
+    color: ${theme.secondary};
+    display: block;
+    justify-content: start;
+    white-space: pre-wrap;
     width: 100%;
     margin: 0;
     justify-content: center;
-
-    @media (min-width: 345px) and (max-width: 767px) {
-        display: block;
-        margin-left: 15%;
-        white-space: pre-wrap;
-        justify-content: start;
-        
-    }
+    top: 50%;
+    transform: translateY(-50%);
+    position: absolute;
+    margin-left: 15%;
+        ${media.desktop} {
+            display: flex;
+            position: absolute;
+     }
 `
 
 const   DIV = styled.div`
@@ -58,18 +55,16 @@ const   DIV = styled.div`
     display: flex;
     justify-content: center;
     margin-top: 5%;
+    width: 100%;
 `
 
 const IMG = styled.img`
-    border-radius: 10px;
-    height: 223px;
+    height: 111px;
     width: 94%;
     object-fit: cover;
     filter: brightness(60%);
-
-    @media (min-width: 345px) and (max-width: 767px) {
-        height: 111px;
+    border-radius: 10px;
+    ${media.desktop} {
+        height: 223px;
     }
 `
-
-
