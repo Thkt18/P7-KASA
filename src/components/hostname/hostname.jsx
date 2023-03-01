6274716a459c605f2c2c8f15f7d9095c9e1e0c89
Thkt18 <110399102+Thkt18@users.noreply.github.com>
2023-03-01 15:50:03 +0100

    HostName mobile first

--- src/components/hostname/hostname.jsx
+++ src/components/hostname/hostname.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components"
+import { media } from '../../style/breakpoint';
+import theme from '../../style/theme';
 
 function HostName({ host }) {
     const hostNameSplitted = host.name.split(' ')
@@ -27,33 +29,32 @@ const DetailsHost = styled.div`
       display: flex;
       align-items: center;
       max-width: 170px;
-      font-family: 'Montserrat', sans-serif;
+      width: 100%;
+      justify-content: flex-end;
 
-      @media (max-width: 767px) and (min-width: 345px) {
-        width: 100%;
-        justify-content: flex-end;
-        
+      ${media.desktop} {
+        width: 150px; 
       }
 `
 
 const Host = styled.div`
-    color: #ff6060;
+    color: ${theme.primary};
     text-align: right;
-    font-size: 18px;
+    font-size: 12px;
     font-weight: 500;
     margin: 25px 15px 0 0;
     line-height: 0.5;
 
-    @media (max-width: 767px) and (min-width: 345px) {
-        font-size: 12px;
+    ${media.desktop} {
+        font-size: 18px;
     }
 `
 
 const HostImg = styled.img`
     border-radius: 50%;
-    height: 64px;
+    height: 32px;
 
-    @media (max-width: 767px) and (min-width: 345px) {
-        height: 32px;
+    ${media.desktop} {
+        height: 64px;
         
 ` 
