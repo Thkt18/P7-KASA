06f7a32c6ddbe4f9828bff48d8f46e868f7f4ce8
Thkt18 <110399102+Thkt18@users.noreply.github.com>
2023-03-01 16:00:50 +0100

    MAJ

--- src/style/theme.js
+++ src/style/theme.js
@@ -2,6 +2,8 @@ const theme = {
     primary: '#FF6060',
     secondary: '#FFFFFF',
     background: '#F7F7F7',
+    backgroundCard: '#F6F6F6',
+    backgroundCollapse: '#D7D7CD',
     font: 'Montserrat',
     maxWidth: '1240px',
     border: '1px solid red',
