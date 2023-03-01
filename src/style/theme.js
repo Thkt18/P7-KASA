9c0c5ab6f4648f8804beb65f1e9c17ca2d65fd2c
Thkt18 <110399102+Thkt18@users.noreply.github.com>
2023-03-01 13:53:33 +0100

    Add theme.js

---
+++ src/style/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+    primary: '#FF6060',
+    seconday: '#FFFFFF',
+    background: '#F7F7F7',
+    font: 'Montserrat',
+    maxWidth: '1200px',
+    border: '1px solid red',
+};
+
+export default theme
